01900a7c4bcc68f4a47ae891effe69de6a0fb812
Dale Nguyen <dale@dalenguyen.me>
2026-04-12 10:49:40 -0400

    fix(e2e): match home page h1

--- web-e2e/src/example.spec.ts
+++ web-e2e/src/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('/login');
+  await page.goto('/');
 
   // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Log in');
+  expect(await page.locator('h1').innerText()).toContain('Can Tax Pro');
 });
